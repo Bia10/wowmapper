5de4ca9b6490f0965ed942cadc08dc0968e4b07c
pferdone <pferdone@42403f9c-0d9a-11df-aac2-bbd07866f500>
2010-02-01 23:17:35 +0000

    added mcnr chunk

---
+++ trunk/src/adt/mcnrchunk_s.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <limits.h>
+#include "../chunk_s.h"
+
+struct McnrChunk_s : Chunk_s {
+  struct Normal_s {
+    float x() const { return _x / 127.0f; }
+    float y() const { return _y / 127.0f; }
+    float z() const { return _z / 127.0f; }
+
+   private:
+    int8_t _x, _y, _z;
+  };
+
+  Normal_s normal[145];
+
+  McnrChunk_s(int32_t memAbsOffset, void *in_buf) {
+    int32_t buf_addr = reinterpret_cast<int32_t>(in_buf);
+    void *dest_addr = reinterpret_cast<void*>(buf_addr + memAbsOffset);
+
+    /* fill MCNR chunk with its real content */
+    memcpy(this, dest_addr, sizeof(McnrChunk_s));
+  }
+};
